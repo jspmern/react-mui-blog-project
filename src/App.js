f15e38beebd3b8017921b008da97f9a75e0b045b
jspmern <134581836+jspmern@users.noreply.github.com>
2024-10-21 23:34:32 +0530

    Basic navBar Done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import MuiAside from "./Component/MuiAside";
 import MuiMain from "./Component/MuiMain";
 import MuiRight from "./Component/MuiRight";
 import { Box, CssBaseline, Stack } from "@mui/material";
+import MuiNav from "./Component/MuiNav";
 
 function App() {
   return (
     <>
       <CssBaseline />
       <Box>
-        {/* //navbar */}
+        <MuiNav />
         <Stack direction={"row"} justifyContent={"space-evenly"}>
           <MuiAside />
           <MuiMain />
--- src/Component/MuiNav.jsx
+++ src/Component/MuiNav.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { Mail } from "@mui/icons-material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  styled,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
+import React from "react";
+
+let CustomToolBar = styled(Toolbar)(({ theme }) => {
+  return {
+    display: "flex",
+    justifyContent: "space-between",
+  };
+});
+let SearchContainer = styled("div")(({ theme }) => {
+  return {
+    backgroundColor: "white",
+    width: "40%",
+    borderRadius: theme.shape.borderRadius,
+  };
+});
+let IconContainer = styled(Box)(({ them }) => {
+  return {
+    display: "flex",
+    gap: "10px",
+    alignItems: "center",
+  };
+});
 function MuiNav() {
-    return (
-        <div>MuiNav</div>
-    )
+  return (
+    <AppBar position="sticky">
+      <CustomToolBar>
+        <Typography variant="h6">UtsavBlog</Typography>
+        <SearchContainer>
+          <TextField
+            id="outlined-basic"
+            label="Search..."
+            variant="outlined"
+            fullWidth
+          />
+        </SearchContainer>
+        <IconContainer>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <NotificationsIcon />
+          </Badge>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://media.licdn.com/dms/image/v2/D5603AQEwvjm5kRAeGg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1714505856986?e=1735171200&v=beta&t=8dN3zP5RAL9hZCw1cT8q6dlFTZUY_w6bzqKIYxcUpv8"
+          />
+        </IconContainer>
+      </CustomToolBar>
+    </AppBar>
+  );
 }
 
-export default MuiNav
+export default MuiNav;
